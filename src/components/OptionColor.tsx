982d153d427ae3c928ea1850cf7635ae414c0b5e
Tyks23 <epic.raider99@gmail.com>
2021-11-25 23:57:40 +0200

    added bugs

--- src/components/OptionColor.tsx
+++ src/components/OptionColor.tsx
@@ -2,15 +2,26 @@ import React from 'react';
 import { isPropertySignature } from 'typescript';
 import './OptionColor.scss';
 
-function OptionColor({ color, name, price, update, target}: {
+function OptionColor({ color, name, price, update, target, isActive}: {
   color: string,
   name: string,
   price: number,
   update: Function,
-  target: string
+  target: string,
+  isActive: boolean
 }) {
+  let classNameTest = 'container-color';
+  if (!isActive) {
+    classNameTest = 'container-color-active';
+  }
+
+  function ReverseBool(variable: boolean){
+    variable != variable;
+    return variable;
+  }
+
   return (
-    <div className="container-color" onClick={() => update(target, price)}>
+    <div className='container-color' onClick={() => {update(target, price); console.log(isActive, classNameTest);}}>
       <div className="color-sample" style={{ backgroundColor: color }} />
       <p className="p-price">+{price}€</p>
       <p>{name}</p>
